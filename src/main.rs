4812a9665d58033d2f23a0f5bceb396a28992ee6
Bhaskar <bhaskar4647@gmail.com>
2020-09-30 18:34:45 -0500

    Guessing game commit

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number! ");
-    let secret_number = radn::thread_rng().gen_range(1,101);
+    let secret_number = rand::thread_rng().gen_range(1,101);
     println!("The secret number is: {}", secret_number);
 
-    println!("Please iput your guess: ");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line!");
 
-    println!("You guessed: {}", guess);
+    loop {
+
+        println!("Please iput your guess: ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
+        let guess: u32 = guess.trim().parse().expect("Please type a numer!");
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number)
+        {
+            Ordering::Less => println!("Too Small!" ),
+            Ordering::Greater => println!("Too Large!" ),
+            Ordering::Equal => {
+                println!("You win!" );
+                break;
+            }
+        }
+    }
 }
